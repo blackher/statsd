73b6fb44cad3b345f4154436eb1a2965e2e7c6a2
shihe@wanglibank.com <shihe@wanglibank.com>
2018-03-29 11:43:23 +0800

    u

--- client.js
+++ client.js
@@ -1,18 +1,32 @@
 'use strict';
 
-const StatsD = require('node-statsd'),
-client = new StatsD({
+const StatsD = require('node-statsd');
+
+const Redis = require('redis');
+
+const statsdClient = new StatsD({
   host: '192.168.11.116',
   port: 8125
 });
 
+const redisClient = Redis.createClient('6379','192.168.11.98');
+
+//主循环取awardList 长度
 setInterval(function () {
-  const responseTime = Math.floor(Math.random() * 1000);
-  client.timing('api', responseTime, function (error, bytes) {
+  
+  //callback  回调
+  redisClient.llen('awardList',function(err, replies){
+    
+    statsdClient.timing('awardListLength', replies, function (error, bytes) {
     if (error) {
       console.error(error);
     } else {
-      console.log(`Successfully sent ${bytes} bytes, responseTime: ${responseTime}`);
+      console.log(`Successfully sent ${bytes} bytes, awardListLength: ${replies}`);
     }
   });
+    
+  });
+  
+  
+  
 }, 1000);
